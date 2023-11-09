472890266b53a6c0f8b54c3b92123ea8e3d1018a
corDomination <thecordonnier@gmail.com>
2023-11-09 09:57:35 -0500

    Improved links

--- js/bstud-controller.js
+++ js/bstud-controller.js
@@ -4,7 +4,8 @@ class BstudController {
     }
 
     async prepare() {
-        const response = await fetch('https://cordomination.github.io/the-thoughts-of-man/data/studies.json');
+        let url = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? 'data/studies.json' : 'https://cordomination.github.io/the-thoughts-of-man/data/studies.json';
+        const response = await fetch(url);
         const json = await response.json();
         this._data = json.entries[0];
         const title = document.querySelector('.page-section-title');
@@ -17,11 +18,12 @@ class BstudController {
             li.textContent = entry;
             contents.appendChild(li);
         }
+        const links = document.querySelector('.page-section-links');
         for(const entry of this._data.links) {
-            const li = document.createElement('a');
-            li.textContent = entry.text;
-            li.src = entry.url;
-            contents.appendChild = li;
+            const link = document.createElement('a');
+            link.textContent = entry.text;
+            link.href = entry.url;
+            links.appendChild(link);
         }
     }
 }
