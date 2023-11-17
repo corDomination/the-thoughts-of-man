3fa80ab950ccc9431e4416d1f678d2d1afbf4c32
corDomination <thecordonnier@gmail.com>
2023-11-16 19:47:46 -0500

    Bstud 2

--- js/bstud-controller.js
+++ js/bstud-controller.js
@@ -7,24 +7,31 @@ class BstudController {
         let url = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? 'data/studies.json' : 'https://cordomination.github.io/the-thoughts-of-man/data/studies.json';
         const response = await fetch(url);
         const json = await response.json();
-        this._data = json.entries[0];
-        const title = document.querySelector('.page-section-title');
-        title.textContent = this._data.name;
-        const caption = document.querySelector('.page-section-caption');
-        caption.textContent = this._data.date;
-        const contents = document.querySelector('.page-section-contents-list');
-        for (const entry of this._data.contents) {
-            const li = document.createElement('li');
-            li.textContent = entry;
-            contents.appendChild(li);
+        this._data = json.entries;
+        const sectionTemplate = document.querySelector('.page-section');
+        const body = document.querySelector('body');
+        for(const data of this._data) {
+            const section = sectionTemplate.cloneNode(true);
+            const title = section.querySelector('.page-section-title');
+            title.textContent = data.name;
+            const caption = section.querySelector('.page-section-caption');
+            caption.textContent = data.date;
+            const contents = section.querySelector('.page-section-contents-list');
+            for (const entry of data.contents) {
+                const li = document.createElement('li');
+                li.textContent = entry;
+                contents.appendChild(li);
+            }
+            const links = section.querySelector('.page-section-links');
+            for(const entry of data.links) {
+                const link = document.createElement('a');
+                link.textContent = entry.text;
+                link.href = entry.url;
+                links.appendChild(link);
+            }
+            body.appendChild(section);
         }
-        const links = document.querySelector('.page-section-links');
-        for(const entry of this._data.links) {
-            const link = document.createElement('a');
-            link.textContent = entry.text;
-            link.href = entry.url;
-            links.appendChild(link);
-        }
+        sectionTemplate.remove();
     }
 }
 
